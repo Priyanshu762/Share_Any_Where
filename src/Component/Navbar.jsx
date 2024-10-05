2c7524298d27e9b903bdadc4c035d97febe18710
Priyanshu kumar <kumarpriyanshu762@gmail.com>
2024-10-05 21:58:14 +0530

    Name changed of website

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className="bg-gray-900 text-white p-4 flex justify-between items-center">
-    <h1 className="text-lg font-bold">My Webpage</h1>
+    <h1 className="text-lg font-bold">ShareAnyWhere</h1>
     <button className="bg-green-500 hover:bg-green-700 px-4 py-2 rounded">
       Share
     </button>
